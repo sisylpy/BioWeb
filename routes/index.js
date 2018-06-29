88bd1ba876f450f3183a8271fb07de768e1710ce
thc <1028256224@qq.com>
2018-06-29 23:39:31 +0800

    视频播放

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,9 @@ router.get('/operation', function(req, res, next) {
 router.get('/products', function(req, res, next) {
   res.render('products');
 });
-//router.get('/detail_detail', function(req, res, next) {
-//	let video_url = req.query.video_url;
-//res.render('detail_detail',{video_url:video_url});
-//});
+router.get('/detail_detail', function(req, res, next) {
+	let video_url = req.query.video_url;
+	let pdf_id = req.query.pdf_id;
+	res.render('detail_detail',{pdf_id:pdf_id,video_url:video_url});
+});
 module.exports = router;
